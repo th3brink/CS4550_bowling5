ace333364a05c0e4769bf19da4b7b31c12c2488a
James Brinkerhoff <th3brink@gmail.com>
2014-04-12 00:23:50 -0600

    finished

--- src/BlackBoard.java
+++ src/BlackBoard.java
@@ -4,48 +4,71 @@ import java.util.List;
 public class BlackBoard {
     static private int scoreIndex = 0;
     static private int frame = 0;
-    static private List<Integer> frames = new ArrayList<Integer>();
+    static private List<List<Integer>> frames = new ArrayList<List<Integer>>();
     static private int totalScore = 0;
     List<Integer> scores = new ArrayList<Integer>();
-    public BlackBoard(List<Integer> list) {
-        scores = list;
 
+    static private int playerCount = 1;
+    static private int playerTurn = 1;
+
+    public BlackBoard(List<Integer> list, int p_count) {
+        scores = list;
+        playerCount = p_count;
+        for (int i = 0; i < playerCount; i++) {
+            frames.add(new ArrayList<Integer>());
+            System.out.println("player " + (i+1) + " created.");
+        }
     }
 
     public void total() {
-        for (int item : frames) {
-            totalScore += item;
+        frame = frame - playerCount+1;
+        System.out.print("Frame " + frame + ": ");
+        int i = 0;
+        for (List<Integer> playerFrame : frames) {
+            i++;
+            totalScore = 0;
+            for (int item : playerFrame) {
+                totalScore += item;
 
+            }
+            System.out.print("Player "+i+": "+totalScore + " | ");
         }
-        System.out.println(totalScore);
-
+        System.out.println("");
     }
 
     public boolean fetch() {
         if (scores.get(scoreIndex) == 10) {
-            frames.add(scores.get(scoreIndex));
-            frames.add(scores.get(scoreIndex + 1));
-            frames.add(scores.get(scoreIndex + 2));
+            frames.get(playerTurn - 1).add(scores.get(scoreIndex));
+            frames.get(playerTurn-1).add(scores.get(scoreIndex + 1));
+            frames.get(playerTurn-1).add(scores.get(scoreIndex + 2));
             scoreIndex++;
             frame++;
 
         } else if (scores.get(scoreIndex) + scores.get(scoreIndex + 1) == 10) {
-            frames.add(scores.get(scoreIndex));
-            frames.add(scores.get(scoreIndex + 1));
-            frames.add(scores.get(scoreIndex + 2));
+            frames.get(playerTurn-1).add(scores.get(scoreIndex));
+            frames.get(playerTurn-1).add(scores.get(scoreIndex + 1));
+            frames.get(playerTurn-1).add(scores.get(scoreIndex + 2));
             scoreIndex += 2;
             frame++;
 
         } else {
-            frames.add(scores.get(scoreIndex));
-            frames.add(scores.get(scoreIndex + 1));
+            frames.get(playerTurn-1).add(scores.get(scoreIndex));
+            frames.get(playerTurn-1).add(scores.get(scoreIndex + 1));
             scoreIndex += 2;
             frame++;
 
         }
-        totalScore = 0;
-        total();
-        return frame == 10;
 
+        if (playerTurn < playerCount) {
+            playerTurn++;
+            fetch();
+
+        } else {
+            total();
+            playerTurn = 1;
+
+        }
+
+        return frame == 10;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,9 @@ public class Main {
         String inString;
         List<Integer> scores = new ArrayList<Integer>();
         Scanner reader = new Scanner(System.in);
+
+        int playerCount = 1;
+
         if (args.length > 0 && args[0] != null) {
             filename = args[0];
 
@@ -29,12 +32,18 @@ public class Main {
                 scores.add(Integer.parseInt(score));
 
             }
+//            System.out.println(scores);
+            if (!scores.isEmpty()) {
+                playerCount = scores.get(0);
+                scores.remove(0);
+            }
+//            System.out.println(scores);
 
         } catch (Exception e) {
             System.out.println("Failed to open file with error: "+e);
 
         }
-        BlackBoard blackboard = new BlackBoard(scores);
+        BlackBoard blackboard = new BlackBoard(scores, playerCount);
         while (!fetched) {
             fetched = blackboard.fetch();
 
